e057e3c9dd985585b8a7b1aa16448e96c1d8facf
contactvarungupta <v.g7404@gmail.com>
2024-05-10 14:40:02 +0400

    7th commit

--- script2.js
+++ script2.js
@@ -1,72 +1,72 @@
-/* Card for the Education.html*/
+/*Card for the Education.html*/
 VanillaTilt.init(document.querySelectorAll(".pr_card_1"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".pr_card_2"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".pr_card_3"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".ex_card_1"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".ex_card_2"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".ex_card_3"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".ed_card_1"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".ed_card_2"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
 
 VanillaTilt.init(document.querySelectorAll(".ed_card_3"), {
-    max: 25,
-    speed: 400,
+    max: 5,
+    speed: 100,
     easing: "cubic-bezier(.05,.80,.60,.99)",
-    perspective: 500,
+    perspective: 1500,
     transition: true
 });
